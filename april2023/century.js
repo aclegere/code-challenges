dbd839a5c89cedafcd25cf5b6427d0f828a99f4d
Anthony Legere <anthonyclegere@gmail.com>
2023-04-16 12:19:22 -0400

    daily challenge

---
+++ april2023/century.js
@@ -0,0 +1,23 @@
+/*
+Introduction
+
+The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+Task
+
+Given a year, return the century it is in.
+Examples
+
+1705 --> 18
+1900 --> 19
+1601 --> 17
+2000 --> 20
+
+*/
+
+
+function century(year) {
+  return Math.ceil(year / 100);
+}
+
+// This one is relatively straight forward, could have also stored
+// this in a variable and returned that. 
